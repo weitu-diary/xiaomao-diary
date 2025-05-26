982634a1b512834342ff57988c698c26c9001372
weitu-diary <cyx5200601@gmail.com>
2025-05-26 09:32:30 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,1 @@
-
-document.getElementById("diary-entry").innerHTML = `
-  <p>今天是我们上线的第一天，小猫终于拥有了自己的日记本。</p>
-  <p>从今天起，我每天都写给你，不许偷懒、不许丢下我。</p>
-`;
+fetch('data.json').then(res => res.json()).then(data => { document.getElementById('diary').textContent = data.text; });
